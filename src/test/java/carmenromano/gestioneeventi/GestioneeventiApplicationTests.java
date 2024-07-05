3a5d560f28b302e2a1383eec5afdaf5059c57213
Carmen Romano <my-9@hotmail.it>
2024-07-05 09:56:29 +0200

    first commit

---
+++ src/test/java/carmenromano/gestioneeventi/GestioneeventiApplicationTests.java
@@ -0,0 +1,13 @@
+package carmenromano.gestioneeventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneeventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
